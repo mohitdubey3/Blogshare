d9a4cdac9af8871bc89d36373b434684d40e1681
Mohit Dubey <mohitkdb@gmail.com>
2026-03-24 10:23:31 +0530

    Update ChangePassword.cshtml.cs

--- Pages/Account/ChangePassword.cshtml.cs
+++ Pages/Account/ChangePassword.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace YourApp.Areas.Identity.Pages.Account
+namespace BlogShare.Areas.Identity.Pages.Account
 {
     public class ChangePasswordModel : PageModel
     {
